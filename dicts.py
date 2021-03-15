cb9cf028ddcf674ffe2a686d2834e78d8b8a1fec
Andrew Viter <andrew.viter@outlook.com>
2021-03-15 13:30:30 -0700

    Initial batch of code from stage 1, data files not included

---
+++ dicts.py
@@ -0,0 +1,23 @@
+mult_value = {
+    'K': 1000,
+    'M': 1000000,
+    'B': 1000000000
+}
+
+indexes = {
+    '100': 'Gross Income', # for gross margin
+    '101': 'Net Income', # for net margin
+    '102': 'Cost of Goods Sold (COGS) incl. D&A' # for operating margin
+}
+
+axis_titles = {
+    '100': 'Gross Margin, %',
+    '101': 'Net Margin, %',
+    '102': 'Operating Margin, %'
+}
+
+main_titles = {
+    '100': 'Gross Margin by Year',
+    '101': 'Net Margin by Year',
+    '102': 'Operating Margin by Year'
+}
