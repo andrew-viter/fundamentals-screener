3b97122a4d4e80ffa585a2e48e4f99fb96d9784e
Andrew Viter <andrew.viter@outlook.com>
2021-03-18 16:19:39 -0700

    Added optional arguments for cleanup and archive

--- fundamentals_screener/file_system_ops/archive.py
+++ fundamentals_screener/file_system_ops/archive.py
@@ -3,21 +3,40 @@ import os
 from shutil import copy
 from glob import glob
 
-def archive():
+def archive(subdir, s=False):
     # list of all image files, if any
     image_file_paths = glob("C:\\Users\\andre\\PythonProjects\\fundamentals_screener\\*.png")
 
     # checks if there are images to archive
     if len(image_file_paths) == 0:
         print("No images to archive")
-    else:
-        with open('../config/config.json') as config_file:
-            config_data = json.load(config_file)
-            archive_path = config_data["png_archive_path"]
-            
-            if os.access(archive_path, os.F_OK):
-                for image in image_file_paths:
-                    copy(image, archive_path)
+        return None
 
-            else:
-                raise Exception("Path to archive location is invalid")
+    # opens config file to get path for archiving
+    with open('../config/config.json') as config_file:
+        config_data = json.load(config_file)
+        archive_path = config_data["png_archive_path"]
+    
+    # if archived files are not going to default directory
+    if not subdir == None:
+        archive_path = os.path.join(archive_path, subdir)
+
+        # checks if '-s' flag is false
+        # if it is true, the subdirectory already exists, so it will write there
+        if s == False:
+            try:
+                os.mkdir(archive_path)
+            except FileExistsError:
+                msg = ' '.join(["The path", archive_path, "already exists. Archive here? [Y/N] "])
+                confirm = input(msg).upper()
+                if confirm == 'N':
+                    return None
+    
+    # a check to ensure the path is accessible
+    if os.access(archive_path, os.F_OK):
+        for image in image_file_paths:
+            copy(image, archive_path)
+
+    # exception raised, os.access only returns true or false
+    else:
+        raise Exception("Path to archive location is invalid")
--- fundamentals_screener/file_system_ops/image_cleanup.py
+++ fundamentals_screener/file_system_ops/image_cleanup.py
@@ -1,7 +1,7 @@
 from glob import glob
 import os
 
-def image_cleanup():
+def image_cleanup(prompt_each=False):
     # list of all image files, if any
     image_file_paths = glob("C:\\Users\\andre\\PythonProjects\\fundamentals_screener\\*.png")
 
@@ -12,6 +12,12 @@ def image_cleanup():
         # removes each image file one by one, since wildcards do not work with os.remove()
         for image in image_file_paths:
             try:
-                os.remove(image)
+                # prompt each is set to true if '-p' flag is passed to 'cleanup'
+                if prompt_each:
+                    confirm = input("Remove " + image + "? [Y/N] ").upper()
+                    if confirm == 'Y':
+                        os.remove(image)
+                else:
+                    os.remove(image)
             except:
-                print("Error while removing file: " + image)
+                print("Error while removing file " + image)
